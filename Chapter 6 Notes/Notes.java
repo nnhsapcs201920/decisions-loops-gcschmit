5f4405a18f4cec7b29861b1508f91c28e9b909ec
Geoff Schmit <gcschmit@gmail.com>
2019-10-29 07:48:08 -0500

    examples of sentinel values, break, continue

--- Chapter 6 Notes/Notes.java
+++ Chapter 6 Notes/Notes.java
@@ -1,6 +1,5 @@
 import java.util.Scanner;
 
-
 /**
  * Write a description of class Notes here.
  *
@@ -19,17 +18,17 @@ public class Notes
          *          if false, skips the body and contines
          */
         int count = 1;                          // initialization
-        
+
         while(count <= 5)                       // condition
         {
             System.out.println(count);          // body
-            
+
             count++;                            // update the loop variable
         }
-        
+
         System.out.println("done");
     }
-    
+
     public static void whileExample2()
     {
         /*
@@ -39,17 +38,17 @@ public class Notes
          *      while(count < 50)
          */
         int count = 1;                          // initialization
-        
+
         while(count != 50)                      // condition
         {
             System.out.println(count);          // body
-            
+
             count += 2;                         // update the loop variable
         }
-        
+
         System.out.println("done");
     }
-    
+
     public static void forExample()
     {
         /*
@@ -62,35 +61,35 @@ public class Notes
          *                                          evaluating the condition again
          */
         for(int count = 1;                      // initialization
-                count <= 5;                     // condition
-                count++)                        // update the loop variable
+        count <= 5;                     // condition
+        count++)                        // update the loop variable
         {
             System.out.println(count);          // body
         }
-        
+
         System.out.println("done");
-        
+
         /*
          * variables declared with the for statement are scoped to the for statement and its body
          */
         //System.out.println("final value of count: " + count);
     }
-    
+
     public static void forExample2()
     {
         int count = 1;
         for(    ;                               // initialization
-                count <= 5;                     // condition
-                count++)                        // update the loop variable
+        count <= 5;                     // condition
+        count++)                        // update the loop variable
         {
             System.out.println(count);          // body
         }
-        
+
         System.out.println("done");
-        
+
         System.out.println("final value of count: " + count);
     }
-    
+
     public static void offByOne()
     {
         /*
@@ -102,18 +101,18 @@ public class Notes
          *                  
          *      "Think! Don't compile and try at random!"
          */
-        
+
         // we want print five "*"
         for(int i = 0;                          // initialization
-                i <= 5; // should be i < 5      // condition
-                i++)                            // update the loop variable
+        i <= 5; // should be i < 5      // condition
+        i++)                            // update the loop variable
         {
             System.out.println("*");            // body
         }
-        
+
         System.out.println("done");
     }
-    
+
     public static void doExample()
     {
         /*
@@ -125,23 +124,114 @@ public class Notes
          *          if false, continues execution after the loop
          */
         int count = 1;                          // initialization
-        
+
         do
         {
             System.out.println(count);          // body
-            
+
             count++;                            // update the loop variable
         }
         while(count <= 5);                      // condition
-        
+
         System.out.println("done");
     }
+
+    public static int sum()
+    {
+        Scanner s = new Scanner(System.in);
+        
+        int sum = 0;
+        int value;
+        
+        do
+        {
+            System.out.print("enter a positive integer (-1 to quit): ");
+            
+            /*
+             * Sentinel Value
+             *      Value (e.g., -1) used to terminate a loop. It is often entered by a user.
+             */
+            value = s.nextInt();
+            
+            // this is a bug! sum is 1 less than it should be
+            sum += value;
+        }
+        while(value != -1);
+        
+        return sum;
+    }
     
+    public static int sum2()
+    {
+        Scanner s = new Scanner(System.in);
+        
+        int sum = 0;
+        int value;
+        
+        do
+        {
+            System.out.print("enter a positive integer (-1 to quit): ");
+            
+            value = s.nextInt();
+            if(value == -1)
+            {
+                /*
+                 * break
+                 *      Immediately exits the inner-most loop. Similar behavior as in switch
+                 *          statements.
+                 */
+                break;
+            }
+            
+            sum += value;
+        }
+        while(value != -1);
+        
+        return sum;
+    }
     
+    public static int sum3()
+    {
+        Scanner s = new Scanner(System.in);
+        
+        int sum = 0;
+        int value  = 0;
+        
+        do
+        {
+            System.out.print("enter a positive integer (-1 to quit): ");
+            
+            /*
+             * The hasNextInt method of the Scanner class returns true if the next token to be read
+             *      is an integer; otherwise, it returns false. It does not consume the token. If
+             *      there are no toekns in the stream, it waits until there are.
+             */
+            if(s.hasNextInt() == false)
+            {
+                System.out.println("\"" + s.next() + "\" is not an integer; try again.");
+                
+                /*
+                 * continue
+                 *      1. immediately skips to the end of the inner-most loop
+                 *      2. re-evaluates the loop condition at that point and continues if the
+                 *          condition is true
+                 */
+                continue;
+            }
+            
+            value = s.nextInt();
+            if(value == -1)
+            {
+                break;
+            }
+            
+            sum += value;
+        }
+        while(value != -1);
+        
+        return sum;
+    }
 }
-
-
-
 
 
 
